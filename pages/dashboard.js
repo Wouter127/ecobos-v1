0f3868db8caa8d4a0d354a7d1b4fc86e9171155c
Wouter127 <r0785520@student.thomasmore.be>
2023-04-08 19:08:33 +0200

    &apos; fix

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -18,7 +18,7 @@ const Dashboard = () => {
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
             <div className="border-b border-gray-200 bg-white p-6">
-              You're logged in!
+              You&apos;re logged in!
             </div>
           </div>
         </div>
